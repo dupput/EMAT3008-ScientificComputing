963268fc9522c382bcd621b37f1ef2e3b38d35d0
Andre <dupput@gmail.com>
2023-02-03 11:32:16 +0000

    Created a unit test file for solvers

---
+++ Helpers/solvers_unittests.py
@@ -0,0 +1,20 @@
+import unittest   # The test framework
+import numpy as np
+
+from solvers import solve_ode
+
+class Test_TestSplitInput(unittest.TestCase):
+    def __init__(self, *args, **kwargs):
+        super(Test_TestSplitInput, self).__init__(*args, **kwargs)
+        self.fun = lambda t, y: 2 - np.exp(-4*t) - 2*y
+        self.t0 = 0
+        self.y0 = 1
+
+    # Testing the function solve_ode
+    def test_1(self):
+        with self.assertRaises(ValueError):
+            solve_ode(self.fun, self.t0, self.y0, method='euler')
+
+    
+if __name__ == '__main__':
+    unittest.main()
