9148307b6985aa1bc8308b4af7cb44713ff08c38
Andre <dupput@gmail.com>
2023-03-30 08:33:47 +0100

    More tests

--- Helpers/bvp_unittests.py
+++ Helpers/bvp_unittests.py
@@ -92,7 +92,9 @@ class TestBVP(unittest.TestCase):
 
         self.assertAlmostEqual(velocity, 34.5)
 
+
     def test_example_2(self):
+        exact = np.exp(-0.2 * np.pi**2)
         a = 0
         b = 1
         alpha = 0
@@ -108,11 +110,59 @@ class TestBVP(unittest.TestCase):
         t_final = 2
 
         t, dt, C = bvp.time_discretization(t_boundary, t_final, dt)
-        solution, t = bvp.explicit_euler(t)
+        solution = bvp.explicit_euler(t)
 
         idx = np.where(bvp.x_values == 0.5)[0][0]
         numeric = solution[idx, -1]
+        
+        assert np.isclose(numeric, exact, atol=1e-2)
+
+    def test_example_3(self):
         exact = np.exp(-0.2 * np.pi**2)
+        a = 0
+        b = 1
+        alpha = 0
+        beta = 0
+        N = 100
+        D = 0.1
+
+        f_fun = lambda x, t: np.sin(np.pi * x)
+        bvp = BVP(a, b, N, alpha, beta, D=D, condition_type='Dirichlet', f_fun=f_fun)
+
+        t_boundary = 0
+        dt = 0.0001
+        t_final = 2
+
+        t, dt, C = bvp.time_discretization(t_boundary, t_final, dt)
+        solution = bvp.implicit_euler(t)
+
+        idx = np.where(bvp.x_values == 0.5)[0][0]
+        numeric = solution[idx, -1]
+        
+        assert np.isclose(numeric, exact, atol=1e-2)
+
+    def test_example_4(self):
+        exact = np.exp(-0.2 * np.pi**2)
+        a = 0
+        b = 1
+        alpha = 0
+        beta = 0
+        N = 100
+        D = 0.1
+
+        f_fun = lambda x, t: np.sin(np.pi * x)
+        bvp = BVP(a, b, N, alpha, beta, D=D, condition_type='Dirichlet', f_fun=f_fun)
+
+        t_boundary = 0
+        dt = 0.0001
+        t_final = 2
+
+        t, dt, C = bvp.time_discretization(t_boundary, t_final, dt)
+        solution = bvp.crank_nicolson(t)
+
+        idx = np.where(bvp.x_values == 0.5)[0][0]
+        numeric = solution[idx, -1]
+        
         assert np.isclose(numeric, exact, atol=1e-2)
         
 
