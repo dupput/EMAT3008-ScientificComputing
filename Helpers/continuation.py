5b8f45f6b24dc03cbf344cb79145b52eeba4249c
Andre <dupput@gmail.com>
2023-04-19 11:23:39 +0200

    Adapted bvp continuation from notebook

--- Helpers/continuation.py
+++ Helpers/continuation.py
@@ -102,6 +102,73 @@ def simple_continuation(function, x0, param_current, variation_param, step_size,
 
     return x_sol, par_sol, dpars
 
+
+def bvp_continuation(bvp, C_start, C_end, N_C, q_base, method='natural_parameter_estimation'):
+    """
+    Performs continuation for a boundary value problem.
+
+    Parameters
+    ----------
+    bvp : BVP object
+        Boundary value problem to be solved.
+    C_start : float
+        Starting value of C.
+    C_end : float
+        Ending value of C.
+    N_C : int
+        Number of steps to take.
+    q_base : function
+        Base q function.
+    method : str, optional
+        Method to use for continuation. The default is 'natural_parameter_estimation'.
+
+    Returns
+    -------
+    Cs : list
+        List of values of C.
+    all_u : list
+        List of solutions.
+
+    Examples
+    --------
+    >>> a = 0
+    >>> b = 1
+    >>> N = 10
+    >>> alpha = 0
+    >>> beta = 0
+    >>> D = 1.0
+    >>> C = 0
+    >>> def q_base(x, u, C=C):
+            return C*np.exp(u)
+    >>> q_fun = lambda x, u: q_base(x, u, C)
+
+    >>> bvp = BVP(a, b, N, alpha, beta, condition_type='Dirichlet', q_fun=q_fun, D=D)
+    >>> Cs, all_u = bvp_continuation(bvp, 0, 4, 200, q_base)
+    """
+
+    if method == 'natural_parameter_estimation':
+        Cs = np.linspace(C_start, C_end, N_C)
+
+    max_u = np.zeros(len(Cs))
+    u_array = np.ones(N-1)
+    all_u = np.zeros((N_C, N+1))
+
+    fig = go.Figure()
+    for C in Cs:
+        # Redefine the q function with the new value of C in terms of q
+        q = lambda x, u: q_base(x, u, C)
+        bvp.q_fun = q
+    
+        u_guess = np.zeros(N-1)    
+        u_array, success = bvp.solve_bvp(u_guess, 'nonlinear')
+
+        if success:
+            all_u[Cs == C, :] = u_array
+    return Cs, all_u
+
+
+
+
 if __name__ == '__main__':
     func = lambda t, x, c: [t, x**3 - x + c]
     x0 = 1
